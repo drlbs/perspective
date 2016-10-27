f1b560813340d9623e849bf5faa6da9e44743888
Andrew J. Pounds <pounds_aj@mercer.edu>
2016-10-27 08:30:03 -0400

    fixed another typo in prototypes.h

--- prototypes.h
+++ prototypes.h
@@ -3,12 +3,12 @@
 
 #include "structs.h"
 
-void init(void) 
-void drawAxes(int length)
-void defineBox( box *face )
-void drawBox( struct box *face )
-void display(void)
-void reshape (int w, int h)
+void init(void);
+void drawAxes(int length);
+void defineBox( box *face );
+void drawBox( struct box *face );
+void display(void);
+void reshape (int w, int h);
 
 /*
 void init(void) 

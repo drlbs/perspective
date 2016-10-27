8091237d05ba1f1a6dcd997450eeb234244cbf41
Andrew J. Pounds <pounds_aj@mercer.edu>
2016-10-27 08:10:14 -0400

    added a comment to opengl.h

--- opengl.h
+++ opengl.h
@@ -1,6 +1,9 @@
 #ifndef OPENGL 
 #define OPENGL 
 
+// Gather all of these here so we can set them easily on systems that locate
+// the OpenGL header files in non-standard locations.
+//
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
